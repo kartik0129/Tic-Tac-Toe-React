0ca708a394c28c898fdb9d0fa42d598696030f13
Kartik Gupta <cooldude.kartik.123@gmail.com>
2024-10-02 22:19:47 +0530

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 import Player from "./components/Player";
+import { useState } from "react";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
+  function handleSelectSqaure() {
+    setActivePlayer((currActivePlayer) => {
+      return currActivePlayer == "X" ? "O" : "X";
+    });
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        GAME BOARD
+        <GameBoard
+          onSelectSquare={handleSelectSqaure}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
+      <Log />
     </main>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
-export default function Player({ name, symbol }) {
-    const [isEdit, setIsEdit] = useState(false);
-    function handleEdit() {
-        setIsEdit(!isEdit);
-    }
+export default function Player({ name, symbol, isActive }) {
+  const [isEdit, setIsEdit] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+  function handleEdit() {
+    setIsEdit((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
   return (
     <>
-      <li>
+      <li className={isActive ? "active" : undefined}>
         <span className="player">
           {isEdit ? (
             <span>
-              <input></input>
+              <input
+                type="text"
+                required
+                onChange={handleChange}
+                value={playerName}
+              ></input>
             </span>
           ) : (
-            <span className="player-name">{name}</span>
+            <span className="player-name">{playerName}</span>
           )}
 
           <span className="player-symbol">{symbol}</span>
